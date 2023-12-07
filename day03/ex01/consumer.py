8c2e55eee382d065228c837f6294184b06867e5f
Tagir Enikeev <enikeev.tg@gmail.com>
2023-12-07 12:11:01 +0300

    materials

--- day03/ex01/consumer.py
+++ day03/ex01/consumer.py
@@ -2,6 +2,10 @@ import common as com
 import logging as log
 
 
+log.basicConfig(level=log.INFO, format="\
+                %(asctime)s - %(levelname)s: %(message)s")
+
+
 def get_args() -> list:
     parser = com.ArgumentParser(description="consumer: receiving an\
                                 argument with a list of account numbers")
@@ -15,7 +19,6 @@ def get_args() -> list:
 
 
 def messages_processing(bad_guys_list: list) -> None:
-    log.basicConfig(level=log.INFO, format="%(asctime)s: %(message)s")
     redis_client = com.Redis(host="localhost", port=com.port, db=com.db)
     sub = redis_client.pubsub()
     sub.subscribe(com.channel_name)
@@ -39,13 +42,14 @@ def main():
     bad_guys = get_args()
     args_check = True
     for el in bad_guys:
-        if el.isdigit() is False:
+        if el.isdigit() is False or len(el) != 10:
             args_check = False
+
 
     if args_check is True:
         messages_processing(bad_guys)
     else:
-        print("incorrect element(s) of bad guys' account numbers list")
+        log.warning("incorrect element(s) of bad guys' account numbers list")
 
 
 if __name__ == "__main__":
